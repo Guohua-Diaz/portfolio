a23a486c69b3460b84d8bf9528df46bcc71863b4
Guohua-Diaz <102787056+Guohua-Diaz@users.noreply.github.com>
2022-06-21 20:05:03 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+
+
+# Quick Preview - Clone, compile and run locally
+#Website: http://guohua-diaz.surge.sh
+
+## How To Use
+
+- Clone this repo
+- Compile
+- Run locally use command from gitbash:
+$npm run dev
+- Deploy to surge
+#$npm run deploy
+
+## Tech Stack Used 
+- Next.js
+- React.js
+- TailwindCSS
+
+
+
+
+
+
+
---
+++ yourData.js
@@ -0,0 +1,70 @@
+export default {
+  //(Please Do Not Remove The comma(,) after every variable)
+  //Change The Website Template
+
+  //   Header Details ---------------------
+  name: "Guohua",
+  headerTaglineOne: "Hello 👋",
+  headerTaglineTwo:
+    "I'm Guohua Diaz.",
+
+  socials: [
+    { title: "Github", link: "https://github.com/Guohua-Diaz/Guohua-Diaz"},
+    { title: "Linkedin", link: "https://www.linkedin.com/feed/" },
+    { title: "Email", link: "mailto:hanbenman222@gmail.com" },
+  ],
+
+  // End Header Details -----------------------
+
+  // Work Section ------------------------
+  projects: [
+    {
+      title: "Project One", //Project Title - Add Your Project Title Here
+      description: "Pac-Man",
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "./images/PacMan1.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/Guohua-Diaz/Pac-Man",
+      changeImageSize: true,
+    },
+    {
+      title: "Project Two", //Project Title - Add Your Project Title Here
+      description: "PacMen_Factory",
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "./images/pacmen-factory 5.gif",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/Guohua-Diaz/PacMen_Factory",
+    },
+    {
+      title: "Project Three", //Project Title - Add Your Project Title Here
+      description: "Eyes-Exercise",
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "./images/eye-movement.jpg",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/Guohua-Diaz/Eyes-Exercise",
+    },
+    {
+      title: "Project Four", //Project Title - Add Your Project Title Here
+      description: "Real_Time_Bus_Tracker",
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "./images/Mit to Harvard.jpg",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/Guohua-Diaz/Real_Time_Bus_Tracker",
+    },
+  ],
+
+  // End Work Section -----------------------
+
+  // About Secton --------------
+  aboutpara:
+    "Hi, my name is Guohua, Diaz. I am a wife and I have two lovely dogs. I live in west part of Tampa, FL. This city is called New Port Richey. I really enjoy the weather of Florida. I used work in spa wellness industry last 7years. We all experienced the Covid-19 in 2020. This event has triggered me and ultimately change my career. Today, here I am. I am currently learning MIT xPRO Professional Certificate in Coding: Full Stack Development with MERN. I am so excited and passionate to obtain the knowledge in this field.",
+  
+  
+    //   End About Section ---------------------
+};
+
+// Thanks for using this template, I would love to hear from you contact me at hello@chetanverma.com
